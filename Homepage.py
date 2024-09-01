33485c15987721ab209992f3f6edbf604020cd31
edenede2 <131803027+edenede2@users.noreply.github.com>
2024-09-01 09:21:54 +0300

    add some paragraph to the homepage

--- Homepage.py
+++ Homepage.py
@@ -7,3 +7,16 @@ st.set_page_config(
 
 st.title("Homepage")
 st.sidebar.success("Welcome to the homepage!")
+
+st.divider()
+
+st.header("Let's get started!")
+
+st.write(
+    """
+    Welcome to the homepage of FitBit code execution project! 🚀
+
+    Select the page you want to navigate to from the sidebar.
+
+    """
+)

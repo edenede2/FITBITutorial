60b66e9d39fd9d7c6868988c542616ffeb77bc36
edenede2 <131803027+edenede2@users.noreply.github.com>
2024-09-01 09:19:52 +0300

    write some into the first steps page

--- pages/2_🤠_FirstSteps.py
+++ pages/2_🤠_FirstSteps.py
@@ -1,1 +1,21 @@
 import streamlit as st
+
+
+st.title("First steps")
+
+st.divider()
+
+st.header("Lets get started!")
+
+st.write(
+    """
+    Welcome to the first steps of your project! 🚀
+
+    In this section, we will guide you through the initial steps to set up your project.
+
+    We will cover the following topics:
+    - Setting up your project Google Drive folders
+    - Creating a new Subject Dates csv file
+    
+    """
+)

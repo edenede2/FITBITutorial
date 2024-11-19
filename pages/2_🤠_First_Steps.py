dac6b1415660182f5f582635a0ec332d0d87f90f
edenede2 <131803027+edenede2@users.noreply.github.com>
2024-11-19 11:23:21 +0200

    s

--- pages/2_🤠_First_Steps.py
+++ pages/2_🤠_First_Steps.py
@@ -131,7 +131,6 @@ st.write(
 note_container = st.container(border=True)
 note_container.markdown(
     """
-    :red[Note:]
     * The names of the files and folders that we will create in the steps below need to be **exactly** as written in the instructions.
     * The names of the files and folders are **case-sensitive**.
     """
